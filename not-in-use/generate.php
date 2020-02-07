d7b1b36bcc412cebcaa9b8c1672ce7d892a9e96d
yudete <yudesleepy@gmail.com>
2020-02-07 21:18:13 +0900

    Deleted not-in-use files

--- not-in-use/generate.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $url = "./source.php";
- 
-    $buff = file_get_contents($url);
- 
-    $fname = "public/index.html";
-    $fhandle = fopen( $fname, "w");
-    fwrite( $fhandle, $buff);
-    fclose( $fhandle );
